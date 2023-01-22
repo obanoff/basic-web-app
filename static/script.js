87e2e967ea0ef3e569c73f7fc8bed82f34615e59
Yevhen Shliakhovskyi <eugene@Eugenes-MacBook-Air.local>
2023-01-22 20:55:04 +0200

    saving html templates

---
+++ static/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+window.addEventListener('load', function() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  Array.prototype.forEach.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
